34e02e9911ec89e7c07fdfcac5fe4ba3781b4387
ljx2099 <3248055668@qq.com>
2021-01-04 00:55:36 +0800

    最终版本

---
+++ build/prod.config.js
@@ -0,0 +1,12 @@
+
+const uglifiyjswebpackOlugin = require("uglifyjs-webpack-plugin");
+const webpackMerge = require("webpack-merge");
+const baseConfig = require("./base.config");
+module.exports = webpackMerge(baseConfig,{  
+  plugins:[
+    
+    //压缩 
+    new uglifiyjswebpackOlugin()
+  ]
+
+})
